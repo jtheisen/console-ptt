22a88d070632d66255b3b7c52c2ee51f8424c1b1
Jens Theisen <pass-github@zokfotpik.de>
2024-04-29 16:18:41 +0200

    Refactor into files

---
+++ Ptt/Parsing/ParserGuide.cs
@@ -0,0 +1,133 @@
+﻿namespace Ptt;
+
+public class ParserGuide
+{
+    static String BooleanSymbols = "⇒⇐⇔ ,∨∧ =";
+    static String DomainSymbols = "+- */ ^";
+    static String RelationSymbols = "<>≤≥ ⊂⊃ ⊆⊇ ∊∍ ∈∋ ϵ";
+
+    static String QuantizationCharacters = "∀∃";
+
+    static String SymbolLetters =
+        "∀∃∑∏";
+    static String SymbolLetterPrecedencePairings =
+        "∨∨+*";
+
+    static Dictionary<Char, Double> precedences;
+
+    static ParserGuide()
+    {
+        precedences = new Dictionary<Char, Double>();
+
+        {
+            var precedence = 0;
+            foreach (var symbolGroup in DomainSymbols.Split())
+            {
+                ++precedence;
+
+                foreach (var chr in symbolGroup)
+                {
+                    precedences[chr] = precedence;
+                }
+            }
+        }
+
+        {
+            var symbolGroups = BooleanSymbols.Split();
+            for (var i = 0; i < symbolGroups.Length; ++i)
+            {
+                foreach (var chr in symbolGroups[i])
+                {
+                    precedences[chr] = -symbolGroups.Length + i;
+                }
+            }
+        }
+    }
+
+    Double? GetPrecedenceForOperatorCharacter(Char chr)
+    {
+        if (RelationSymbols.IndexOf(chr) != -1)
+        {
+            return 0;
+        }
+
+        if (precedences.TryGetValue(chr, out var precedence))
+        {
+            return precedence;
+        }
+
+        return null;
+    }
+
+    public Double GetPrecedence(ReadOnlySpan<Char> op)
+    {
+        if (op.Length == 1)
+        {
+            var chr = op[0];
+
+            if (GetPrecedenceForOperatorCharacter(chr) is Double precedence)
+            {
+                return precedence;
+            }
+        }
+
+        throw new Exception($"Unknown operator {new String(op)}");
+    }
+
+    public Boolean IsSymbolLetter(Char c, out Double precedence)
+    {
+        precedence = 0;
+
+        var i = SymbolLetters.IndexOf(c);
+        
+        if (i >= 0)
+        {
+            var pairing = SymbolLetterPrecedencePairings[i];
+
+            if (GetPrecedenceForOperatorCharacter(pairing) is Double p)
+            {
+                precedence = p;
+            }
+            else
+            {
+                throw new Exception($"Unkown pairing for '{c}'");
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public Boolean IsQuantizationSymbol(ReadOnlySpan<Char> op, out Double precedence)
+    {
+        precedence = 0;
+
+        if (op.Length == 1)
+        {
+            var chr = op[0];
+
+            return IsSymbolLetter(chr, out precedence);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public Boolean IsBooleanQuantizationSymbol(ReadOnlySpan<Char> op)
+    {
+        if (op.Length == 1)
+        {
+            var chr = op[0];
+
+            return QuantizationCharacters.IndexOf(chr) >= 0;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Ptt/Parsing/ParserTypes.cs
@@ -0,0 +1,164 @@
+﻿using System.Text;
+
+namespace Ptt;
+
+public enum InputCharClass
+{
+    Unset,
+    Unknown,
+    Unsupported,
+
+    Space,
+    Comment,
+
+    Eof,
+
+    Comma,
+    Semikolon,
+    Colon,
+    Dot,
+
+    Letter,
+    SymbolLetter,
+    Operator,
+    OpeningBrace,
+    ClosingBrace,
+    OpeningBracket,
+    ClosingBracket
+}
+
+public struct InputToken
+{
+    public InputCharClass cls;
+    public String line;
+    public Int32 lineI;
+    public Int32 colI;
+    public Int32 endI;
+
+    public String TokenString => line[colI..endI];
+    public ReadOnlySpan<Char> TokenSpan => line.AsSpan()[colI..endI];
+
+    public override String ToString() => line is not null ? line.Length == 0 ? $":empty {cls}:" : TokenString : $":{cls}:";
+
+    public static implicit operator Boolean(InputToken token) => token.cls != InputCharClass.Unset;
+
+    public void Clear() => cls = InputCharClass.Unset;
+}
+
+public static class InputCharClassExtensions
+{
+    public static Boolean IsSubstantial(this InputCharClass cls, Boolean includeEof = true)
+    {
+        switch (cls)
+        {
+            case InputCharClass.Space:
+            case InputCharClass.Comment:
+                return false;
+            case InputCharClass.Eof:
+                return includeEof;
+            default:
+                return true;
+        }
+    }
+
+    public static Boolean IsSingleCharacter(this InputCharClass cls)
+    {
+        switch (cls)
+        {
+            case InputCharClass.SymbolLetter:
+            case InputCharClass.Comma:
+            case InputCharClass.Semikolon:
+            case InputCharClass.Colon:
+            case InputCharClass.Dot:
+            case InputCharClass.Operator:
+            case InputCharClass.OpeningBracket:
+            case InputCharClass.ClosingBracket:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
+
+public class ParsingException : Exception
+{
+    public ParsingException(String message)
+        : base(message)
+    {
+    }
+}
+
+public class ParsedResult
+{
+    public static ParsedResult EmptyResult = new ParsedResult();
+
+    public virtual Boolean IsEmpty => true;
+
+    public static implicit operator Boolean(ParsedResult self) => !self.IsEmpty;
+}
+
+public class ParsedChain : ParsedResult
+{
+    public required List<(ParsedResult item, InputToken op)> constituents;
+
+    public override Boolean IsEmpty => constituents.Count == 0;
+
+    public override String ToString()
+    {
+        var sb = new StringBuilder();
+        foreach (var part in constituents)
+        {
+            var (item, op) = part;
+
+            if (op)
+            {
+                sb.Append(op.TokenSpan);
+            }
+
+            if (item is ParsedAtom atom)
+            {
+                sb.Append(atom.token.TokenSpan);
+            }
+            else
+            {
+                sb.Append('(');
+                sb.Append(item.ToString());
+                sb.Append(')');
+            }
+        }
+        return sb.ToString();
+    }
+}
+
+public class ParsedAtom : ParsedResult
+{
+    public required InputToken token;
+
+    public override Boolean IsEmpty => false;
+
+    public override String ToString() => token.ToString();
+}
+
+public class ParsedQuantization : ParsedResult
+{
+    public required InputToken token;
+
+    public required ParsedResult head;
+    public required ParsedResult body;
+
+    public Boolean IsBraceExpression => token.cls == InputCharClass.OpeningBrace;
+
+    public override Boolean IsEmpty => false;
+
+    public override String ToString()
+    {
+        if (IsBraceExpression)
+        {
+            return $"{{ {head}: {body} }}";
+        }
+        else
+        {
+            return $"{token} {head}: {body}";
+        }
+    }
+}
